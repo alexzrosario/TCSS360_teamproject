7e4c9848b4041c380222e4ec2dc5f291486e4c40
Dylan Frederick <dylanf47@uw.edu>
2022-04-27 17:59:16 -0700

    Added Priestess class for Hero

---
+++ src/Priestess.java
@@ -0,0 +1,17 @@
+public class Priestess extends Hero{
+
+    public Priestess(String theName, int theHitPoints, int theMinDam, int theMaxDam, int theAttackSpeed, int theHitChance) {
+        super(theName, 75, 25, 45, 5, 0.7);
+    }
+
+    @Override
+    void attack() {
+
+    }
+
+    @Override
+    void specialSkll() {
+
+    }
+}
+
