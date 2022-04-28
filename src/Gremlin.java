6227c22ae3a0f183383f4742f2d7298e576ae0b8
Dylan Frederick <dylanf47@uw.edu>
2022-04-27 18:18:01 -0700

    Added Gremlin for Monster

---
+++ src/Gremlin.java
@@ -0,0 +1,15 @@
+public class Gremlin extends Monster{
+    public Gremlin(String theName, int theHitPoints, int theMinDam, int theMaxDam, int theAttackSpeed, double theHitChance, double theHealChance) {
+        super("Gremlin", 70, 15, 30, 5, 0.8, 0.4, 20, 40);
+    }
+
+    @Override
+    void attack() {
+
+    }
+
+    @Override
+    void heal() {
+
+    }
+}
