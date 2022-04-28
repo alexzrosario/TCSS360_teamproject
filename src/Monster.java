2bf59353f91fc433d18e381f800cc0f325050782
Dylan Frederick <dylanf47@uw.edu>
2022-04-27 18:16:16 -0700

    Added Ogre for Monster

--- src/Monster.java
+++ src/Monster.java
@@ -1,9 +1,13 @@
 abstract class Monster extends DungeonCharacter{
     double myHealChance;
+    int myMinHeal;
+    int myMaxHeal;
 
-    public Monster(String theName, int theHitPoints, int theMinDam, int theMaxDam, int theAttackSpeed, double theHitChance, double theHealChance) {
+    public Monster(String theName, int theHitPoints, int theMinDam, int theMaxDam, int theAttackSpeed, double theHitChance, double theHealChance, int theMinHeal, int theMaxHeal) {
         super(theName, theHitPoints, theMinDam, theMaxDam, theAttackSpeed, theHitChance);
         this.myHealChance = theHealChance;
+        this.myMinHeal = theMinHeal;
+        this.myMaxHeal = theMaxHeal;
     }
 
     abstract void heal();
---
+++ src/Ogre.java
@@ -0,0 +1,15 @@
+public class Ogre extends Monster{
+    public Ogre(String theName, int theHitPoints, int theMinDam, int theMaxDam, int theAttackSpeed, double theHitChance, double theHealChance) {
+        super("Ogre", 200, 30, 60, 2, 0.6, 0.1, 30, 60);
+    }
+
+    @Override
+    void attack() {
+
+    }
+
+    @Override
+    void heal() {
+
+    }
+}
