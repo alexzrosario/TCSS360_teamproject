c69397bbba8144711ea8643fc294398f7f9137b8
Dylan Frederick <dylanf47@uw.edu>
2022-05-02 19:35:30 -0700

    Added code from the Alexz branch for DungeonCharacter, specifically the attack methods

--- src/DungeonCharacter.java
+++ src/DungeonCharacter.java
@@ -7,6 +7,7 @@ abstract class DungeonCharacter {
     int myMaxDam;
     int myAttackSpeed;
     double myHitChance;
+    boolean myAlive = true;
     final int MAX_HEALTH;
 
     public DungeonCharacter(String theName, int theHitPoints, int theMinDam, int theMaxDam, int theAttackSpeed, double theHitChance) {
@@ -19,7 +20,68 @@ abstract class DungeonCharacter {
         this.myHitChance = theHitChance;
     }
 
-    int attack() {
+    public void basicAttack(final DungeonCharacter theTarget){
+        int attacks = 1;
+        if(this.getMyAttackSpeed() > theTarget.getMyAttackSpeed()){
+            if(this.getMyAttackSpeed() == 2 * theTarget.getMyAttackSpeed()){
+                attacks = 2;
+            } else if(this.getMyAttackSpeed() == 3 * theTarget.getMyAttackSpeed()){
+                attacks = 3;
+            }
+            for (int i = 0; i < attacks; i++) {
+                attackValue(theTarget);
+            }
+        } else {
+            attackValue(theTarget);
+        }
+    }
+
+    public int attackValue(final DungeonCharacter theTarget) {
+        Random r = new Random();
+        int damageRoll = 0;
+        if (Math.random() <= this.getMyHitChance()) {
+            damageRoll = r.nextInt(this.getMyMinDam(), this.getMyMaxDam() + 1);
+        }
+        if (theTarget instanceof Hero) {
+            if (Math.random() <= ((Hero) theTarget).getMyBlockChance()) {
+                damageRoll = 0;
+                //for testing - block
+                System.out.println("Attack blocked by hero.");
+            } else {
+                theTarget.updateHealth(damageRoll);
+            }
+        }else if(theTarget instanceof Monster) {
+            if(Math.random() <= ((Monster) theTarget).getMyHealChance()){
+                //damageRoll = 0;
+                //for testing - heal
+                System.out.println("Monster Healed.");
+                ((Monster) theTarget).heal();
+            }else{
+                theTarget.updateHealth(damageRoll);
+            }
+        }else{
+            theTarget.updateHealth(damageRoll);
+        }
+        //for testing - correct damage
+        System.out.println(damageRoll);
+        return damageRoll;
+    }
+
+    public void updateHealth(final int theDamage){
+        this.setMyHitPoints(this.getMyHitPoints() - theDamage);
+        if(this.getMyHitPoints() <= 0){
+            setMyAlive();
+        }
+    }
+    public void setMyAlive() {
+        this.myAlive = false;
+    }
+
+    public boolean getMyAlive() {
+        return myAlive;
+    }
+
+    /*int attack() {
         Random r = new Random();
         int damageRoll = 0;
         for (int i = 0; i < myAttackSpeed; i += 2) {
@@ -29,7 +91,7 @@ abstract class DungeonCharacter {
             }
         }
         return damageRoll;
-    }
+    }*/
 
     public String getMyName() {
         return myName;
@@ -77,5 +139,9 @@ abstract class DungeonCharacter {
 
     public void setMyHitChance(int myHitChance) {
         this.myHitChance = myHitChance;
+    }
+
+    public int getMAX_HEALTH() {
+        return MAX_HEALTH;
     }
 }
