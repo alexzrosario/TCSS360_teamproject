6ac5bab1336c4a479130a6d0eebe10313b80b782
Bronson <bronson.costa@outlook.com>
2022-05-02 17:32:00 -0700

    Merged from Dylan, added extra variables to help Dungeon class

---
+++ src/main/DungeonMain/Room.java
@@ -0,0 +1,66 @@
+package main.DungeonMain;
+
+import java.sql.SQLOutput;
+import java.util.Random;
+
+public class Room {
+    Room myNorthRoom;
+    Room myEastRoom;
+    Room mySouthRoom;
+    Room myWestRoom;
+    boolean hasMultipleItems = false;
+    boolean hasPit = false;
+    boolean hasVisionPotion = false;
+    boolean hasHealPotion = false;
+    boolean hasMonster = false;
+    boolean hasPillar = false;
+    boolean isEntrance = false;
+    boolean isExit = true;
+
+    public Room() {
+        Random r = new Random();
+        int items = 0;
+        int itemRoll = r.nextInt(10) + 1; // Determining if the room will contain a pit
+        if (itemRoll == 1) {
+            hasPit = true;
+            items++;
+        }
+        itemRoll = r.nextInt(10) + 1; // Determining if the room will contain a vision potion
+        if (itemRoll == 1) {
+            hasVisionPotion = true;
+            items++;
+        }
+        itemRoll = r.nextInt(10) + 1; // Determining if the room will contain a healing potion
+        if (itemRoll == 1) {
+            hasHealPotion = true;
+            items++;
+        }
+        itemRoll = r.nextInt(10) + 1; // Determine if room has monster
+        if ( itemRoll == 1) {
+            hasMonster = true;
+        }
+        if (items > 1) hasMultipleItems = true;
+    }
+
+    public String toString() {
+        String centerValue = " ";
+        String roomString = "";
+        if (myNorthRoom != null) roomString += "*-*\n";
+        else roomString += "***\n";
+
+        if (myWestRoom != null) roomString += "|";
+        else roomString += "*";
+
+        // insert block for center value
+        roomString += " ";
+        //
+
+        if (myEastRoom != null) roomString += "|\n";
+        else roomString += "*\n";
+
+        if (mySouthRoom != null) roomString += "*-*";
+        else roomString += "***";
+
+        return roomString;
+    }
+}
