76f6fe6087b20f64954b6019d28e2946a005f411
Bronson <bronson.costa@outlook.com>
2022-05-04 14:34:13 -0700

    Changed Dungeon class according to the changes made to Room, using Setters and Getters, also creating 4 separate Pillar methods for each OOP.

--- src/main/DungeonMain/Dungeon.java
+++ src/main/DungeonMain/Dungeon.java
@@ -1,12 +1,12 @@
 package main.DungeonMain;
 
 import java.util.Random;
+
 public class Dungeon {
     private Room[][] dungeon;
     private Room heroPosition;
     private int startingRow;
     private int startingCol;
-    private int pillarCount = 4;
 
     public Dungeon() {
         dungeon = new Room[5][5];
@@ -26,10 +26,10 @@ public class Dungeon {
         int col = r.nextInt(5);
         startingRow = row;
         startingCol = col;
-        dungeon[row][col].hasEntrance = true;
-        dungeon[row][col].hasHealPotion = false;
-        dungeon[row][col].hasVisionPotion = false;
-        dungeon[row][col].hasPit = false;
+        dungeon[row][col].setEntrance(true);
+        dungeon[row][col].setHasHealPotion(false);
+        dungeon[row][col].setHasVisionPotion(false);
+        dungeon[row][col].setHasPit(false);
     }
 
     public void setHeroStart() {
@@ -43,25 +43,67 @@ public class Dungeon {
         if (dungeon[row][col] == dungeon[startingRow][startingCol]) {
             setExit();
         } else {
-            dungeon[row][col].hasExit = true;
-            dungeon[row][col].hasHealPotion = false;
-            dungeon[row][col].hasVisionPotion = false;
-            dungeon[row][col].hasPit = false;
+            dungeon[row][col].setExit(true);
+            dungeon[row][col].setHasHealPotion(false);
+            dungeon[row][col].setHasVisionPotion(false);
+            dungeon[row][col].setHasPit(false);
         }
     }
 
-    public void setPillars() {
+    public void setAbstractionPillar() {
         Random r = new Random();
-        while(pillarCount > 0) {
-            int row = r.nextInt(5);
-            int col = r.nextInt(5);
-            if(dungeon[row][col].hasPillar || dungeon[row][col].hasEntrance || dungeon[row][col].hasExit) {
-                setPillars();
-            } else {
-                dungeon[row][col].hasPillar = true;
-            }
-            pillarCount--;
+        int row = r.nextInt(5);
+        int col = r.nextInt(5);
+        if(dungeon[row][col].isEntrance() || dungeon[row][col].isExit() ||
+                dungeon[row][col].isHasEncapsulationPillar() || dungeon[row][col].isHasInheritancePillar() ||
+                dungeon[row][col].isHasPolymorphismPillar()) {
+            setAbstractionPillar();
+        } else {
+            dungeon[row][col].setHasAbstractionPillar(true);
         }
+    }
+
+    public void setInheritancePillar() {
+        Random r = new Random();
+        int row = r.nextInt(5);
+        int col = r.nextInt(5);
+        if(dungeon[row][col].isEntrance() || dungeon[row][col].isExit() ||
+                dungeon[row][col].isHasEncapsulationPillar() || dungeon[row][col].isHasAbstractionPillar() ||
+                dungeon[row][col].isHasPolymorphismPillar()) {
+            setInheritancePillar();
+        } else {
+            dungeon[row][col].setHasInheritancePillar(true);
+        }
+    }
+
+    public void setEncapsulationPillar() {
+        Random r = new Random();
+        int row = r.nextInt(5);
+        int col = r.nextInt(5);
+        if(dungeon[row][col].isEntrance() || dungeon[row][col].isExit() ||
+                dungeon[row][col].isHasAbstractionPillar() || dungeon[row][col].isHasInheritancePillar() ||
+                dungeon[row][col].isHasPolymorphismPillar()) {
+            setEncapsulationPillar();
+        } else {
+            dungeon[row][col].setHasEncapsulationPillar(true);
+        }
+    }
+
+    public void setPolymorphismPillar() {
+        Random r = new Random();
+        int row = r.nextInt(5);
+        int col = r.nextInt(5);
+        if(dungeon[row][col].isEntrance() || dungeon[row][col].isExit() ||
+                dungeon[row][col].isHasEncapsulationPillar() || dungeon[row][col].isHasInheritancePillar() ||
+                dungeon[row][col].isHasAbstractionPillar()) {
+            setPolymorphismPillar();
+        } else {
+            dungeon[row][col].setHasPolymorphismPillar(true);
+        }
+    }
+
+    public void setMonsters() {
+
     }
 
     public String toString() {
