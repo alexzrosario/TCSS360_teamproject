03ef35dd7215be46597d09c128c85d40ff45c9fc
Dylan Frederick <dylanf47@uw.edu>
2022-05-07 15:27:18 -0700

    Fixed bugs that caused only west rooms to be updated

--- src/main/DungeonMain/DungeonEXP.java
+++ src/main/DungeonMain/DungeonEXP.java
@@ -53,21 +53,21 @@ public class DungeonEXP {
             //4 - WEST
             if(currentRow + 1 < myDungeonRows && direction == 1) {
                 nextRow = currentRow + 1;
-                myDungeon[currentRow][currentCol].setMyWestRoom(myDungeon[nextRow][nextCol]);
+                myDungeon[currentRow][currentCol].setMyNorthRoom(myDungeon[nextRow][nextCol]);
                 currentRow = nextRow;
                 //currentRoom.setMyNorthRoom(myDungeon[nextRow][nextCol]);
                 if(!myDungeon[currentRow][currentCol].isVisited()) roomsVisited++;
             }
             else if(currentRow - 1 > -1 && direction == 2) {
                 nextRow = currentRow - 1;
-                myDungeon[currentRow][currentCol].setMyWestRoom(myDungeon[nextRow][nextCol]);
+                myDungeon[currentRow][currentCol].setMySouthRoom(myDungeon[nextRow][nextCol]);
                 currentRow = nextRow;
                 //currentRoom.setMySouthRoom(myDungeon[nextRow][nextCol]);
                 if(!myDungeon[currentRow][currentCol].isVisited()) roomsVisited++;
             }
             else if(currentCol + 1 < myDungeonCols && direction == 3) {
                 nextCol = currentCol + 1;
-                myDungeon[currentRow][currentCol].setMyWestRoom(myDungeon[nextRow][nextCol]);
+                myDungeon[currentRow][currentCol].setMyEastRoom(myDungeon[nextRow][nextCol]);
                 currentCol = nextCol;
                 //currentRoom.setMyEastRoom(myDungeon[nextRow][nextCol]);
                 if(!myDungeon[currentRow][currentCol].isVisited()) roomsVisited++;
