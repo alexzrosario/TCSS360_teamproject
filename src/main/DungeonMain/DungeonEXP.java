0d019bee3628c06814139c25565dad0c0ddc119c
Dylan Frederick <dylanf47@uw.edu>
2022-05-07 14:47:06 -0700

    Added an experimental dungeon class. It is meant to test out an algorithm for generating a dungeon

---
+++ src/main/DungeonMain/DungeonEXP.java
@@ -0,0 +1,99 @@
+package main.DungeonMain;
+
+import java.util.Random;
+
+public class DungeonEXP {
+    private Room[][] myDungeon;
+    private Room myHeroPosition;
+    //private Room myCurrentGeneratingRoom;
+    private int myDungeonRows;
+    private int myDungeonCols;
+    private int myStartingRow;
+    private int myStartingCol;
+    /*private enum Directions{
+        NORTH,
+        SOUTH,
+        EAST,
+        WEST;
+    }*/
+
+    public DungeonEXP() {
+        myDungeonRows = 5;
+        myDungeonCols = 5;
+        myDungeon = new Room[myDungeonRows][myDungeonCols];
+    }
+
+    public void buildDungeonArray() {
+        for(int row = 0; row < myDungeonRows; row++) {
+            for(int col = 0; col < myDungeonCols; col++) {
+                myDungeon[row][col] = new Room();
+            }
+        }
+    }
+
+    public void generateDungeon() {
+        Random r = new Random();
+        int roomsVisited = 1;
+        setEntrance();
+        int currentRow = myStartingRow;
+        int currentCol = myStartingCol;
+        int nextRow = myStartingRow;
+        int nextCol = myStartingCol;
+        Room currentRoom = myDungeon[currentRow][currentCol];
+
+        while (roomsVisited < myDungeonRows*myDungeonCols) {
+            //Temporary
+            //Change later
+            int direction = r.nextInt(4) + 1;
+            //1 - NORTH
+            //2 - SOUTH
+            //3 - EAST
+            //4 - WEST
+            if(currentRow + 1 < myDungeonRows && direction == 1) {
+                nextRow = currentRow + 1;
+                currentRoom.setMyNorthRoom(myDungeon[nextRow][nextCol]);
+                if(currentRoom.isVisited()) roomsVisited++;
+            }
+            else if(currentRow - 1 > -1 && direction == 2) {
+                nextRow = currentRow - 1;
+                currentRoom.setMySouthRoom(myDungeon[nextRow][nextCol]);
+                if(currentRoom.isVisited()) roomsVisited++;
+            }
+            else if(currentCol + 1 < myDungeonCols && direction == 3) {
+                nextCol = currentCol + 1;
+                currentRoom.setMyEastRoom(myDungeon[nextRow][nextCol]);
+                if(currentRoom.isVisited()) roomsVisited++;
+            }
+            else if(currentCol - 1 > -1 && direction == 4) {
+                nextCol = currentCol - 1;
+                currentRoom.setMyWestRoom(myDungeon[nextRow][nextCol]);
+                currentRoom = myDungeon[nextRow][nextCol];
+                if(currentRoom.isVisited()) roomsVisited++;
+            }
+        }
+        setExit(currentRow, currentCol);
+    }
+
+    public void setEntrance() {
+        Random r = new Random();
+        int row = r.nextInt(myDungeonRows);
+        int col = r.nextInt(myDungeonCols);
+        myStartingRow = row;
+        myStartingCol = col;
+        myDungeon[row][col].setEntrance(true);
+        myDungeon[row][col].setHasHealPotion(false);
+        myDungeon[row][col].setHasVisionPotion(false);
+        myDungeon[row][col].setHasPit(false);
+        myDungeon[row][col].setMyStringToken("i");
+    }
+
+    public void setExit(int row, int col) {
+        myDungeon[row][col].setExit(true);
+        myDungeon[row][col].setHasHealPotion(false);
+        myDungeon[row][col].setHasVisionPotion(false);
+        myDungeon[row][col].setHasPit(false);
+        myDungeon[row][col].setMyStringToken("O");
+    }
+
+}
+
