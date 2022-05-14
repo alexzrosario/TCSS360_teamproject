132424c737d03c4136daa3ca1017b81d51b05396
Bronson <bronson.costa@outlook.com>
2022-05-13 18:59:56 -0700

    Dungeon GUI Continuation, Attempting to add images

--- src/main/DungeonGUI/DungeonGame.java
+++ src/main/DungeonGUI/DungeonGame.java
@@ -2,20 +2,21 @@ package main.DungeonGUI;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class DungeonGame {
-
     ChoiceController handleChoice = new ChoiceController();
     DungeonUI ui = new DungeonUI();
     DungeonUIManager um = new DungeonUIManager(ui);
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         new DungeonGame();
     }
 
-    public DungeonGame() {
+    public DungeonGame() throws IOException {
         ui.DungeonUI(handleChoice);
     }
+
 
     public class ChoiceController implements ActionListener {
         public void actionPerformed(ActionEvent event) {
@@ -24,7 +25,7 @@ public class DungeonGame {
             switch (choice) {
                 case "start" -> um.nameInputScreen();
                 case "name" -> um.heroSelectScreen();
-                case "hero" -> um.blankScreen();
+                case "hero" -> um.dungeonRoomScreen();
             }
         }
     }
