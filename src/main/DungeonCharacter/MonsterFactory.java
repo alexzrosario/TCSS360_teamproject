068df3174e2d67eb29d1524ea6077047287a9ac1
alexzrosario <alexzrosario@gmail.com>
2022-05-20 22:56:00 -0700

    Fixed monster factory to work with SQL

--- src/main/DungeonCharacter/MonsterFactory.java
+++ src/main/DungeonCharacter/MonsterFactory.java
@@ -1,10 +1,60 @@
 package main.DungeonCharacter;
 
+import org.sqlite.SQLiteDataSource;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class MonsterFactory {
     public Monster createMonster(String theMonster) {
-        if (theMonster.equalsIgnoreCase("ogre")) return new Ogre();
-        else if (theMonster.equalsIgnoreCase("gremlin")) return new Gremlin();
-        else if (theMonster.equalsIgnoreCase("skeleton")) return new Skeleton();
-        else throw new IllegalArgumentException("Illegal Argument; Monster must be an Ogre, Gremlin, or Skeleton");
+        SQLiteDataSource ds = null;
+
+        //establish connection (creates db file if it does not exist :-)
+        try {
+            ds = new SQLiteDataSource();
+            ds.setUrl("jdbc:sqlite:monsters.db");
+        } catch ( Exception e ) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+        String query = "";
+        if (theMonster.equalsIgnoreCase("ogre")) {
+            query = "SELECT * FROM monsters WHERE NAME= 'Ogre' ";
+        } else if (theMonster.equalsIgnoreCase("gremlin")) {
+            query = "SELECT * FROM monsters WHERE NAME= 'Gremlin' ";
+        } else if (theMonster.equalsIgnoreCase("skeleton")) {
+            query = "SELECT * FROM monsters WHERE NAME= 'Skeleton' ";
+        } else {
+            throw new IllegalArgumentException("Illegal Argument; Monster must be an Ogre, Gremlin, or Skeleton");
+        }
+
+        try ( Connection conn = ds.getConnection();
+              Statement stmt = conn.createStatement(); ) {
+
+            ResultSet rs = stmt.executeQuery(query);
+
+            //walk through each 'row' of results, grab data by column/field name
+            // and print it
+            while ( rs.next() ) {
+                String name = rs.getString( "NAME" );
+                int hp = rs.getInt( "HP" );
+                int minDam = rs.getInt( "MIND" );
+                int maxDam = rs.getInt( "MADAM" );
+                int attkSpeed = rs.getInt( "SPEED" );
+                int hitChance = rs.getInt( "HITCHANCE" );
+                int healChance = rs.getInt( "HEALCHANCE" );
+                int minHeal = rs.getInt( "MINHEAL" );
+                int maxHeal = rs.getInt( "MAXHEAL" );
+
+                return new Monster(name,hp,minDam,maxDam,attkSpeed,hitChance,healChance,minHeal,maxHeal);
+            }
+        } catch ( SQLException e ) {
+            e.printStackTrace();
+            System.exit( 0 );
+
+        }
+        return null;
     }
 }
